5cafbb22105f8007752cd171e8cc0ad32b24f4ea
duong lam <duongwert2@gmail.com>
2022-06-06 22:58:41 -0400

    commit 2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-<h1> hi </h1>
+var express = require('express');
+var app = express();
+app.get('/contact', function (req, res){
+	res.send('hello world');
+});
+app.listen(3000, function () {
+	console.log('Example app');
+});
